cabcfcfb59eedd979dd2b05fbbdb64b571c84aa1
Greg Wiley <gwiley@cj.com>
2018-05-31 08:48:31 -0700

    documentation

--- src/Effects.hs
+++ src/Effects.hs
@@ -9,6 +9,21 @@ import qualified Network.AWS.SNS.Publish as SQS
 import qualified Network.AWS as AWS
 import Control.Lens
 
+
+{-
+
+Here are the effectful programs we deliver. Both are under test
+coverage.  The first program depends only on the "EMail" effect.
+That is, it can run in any environment that supplies an
+interpratation of that one effect.
+
+The second places more restrictions on the environment. I depends
+on both "EMail" and "SMS."  It can run in any environment that
+supplies an interpretation of both but it cannot run in an
+environment that is missing an interepration for either of those
+effects.
+
+-}
 notifyInteresting :: (Member EMail effects) => Text -> Eff effects ()
 notifyInteresting = dispatchEMailMessage
 
@@ -19,9 +34,23 @@ notifyEmergency msg = do
     dispatchSMSMessage msg
     dispatchSMSMessage msg
 
-runNotifications :: Eff '[EMail, SMS, IO] a -> IO a
-runNotifications effs = handleEMailWithAWS effs & handleSMSWithAWS & runM
 
+{-
+Effect languages. These are used by the notification programs. They don't
+imply any particular interpretations. The effect model of "Control.
+Monad.Freer" is one of an _authority_ to which programs send requests.
+Thus effects are modeled as data i.e. the effect request to be sent
+
+The "EMail" effect, for instance provides the constructor
+"DispatchEMailMessage" which takes a message as text and returns nothing
+(unit  return). The "EMail" effect could supply more operations by
+defining them as additional constructors.
+
+The "dispatchEmailFunction" is simply a convenience that sends a constructed
+request to the authority. Again, the authority is an abstract effect
+performer. Implementations are not implied by these definitions.
+
+-}
 data EMail a where
     DispatchEMailMessage :: Text -> EMail ()
 
@@ -35,11 +64,22 @@ dispatchSMSMessage :: (Member SMS effects) => Text -> Eff effects ()
 dispatchSMSMessage msg = send $ DispatchSMSMessage msg
 
 
+{-
+
+Effect handlers. In order to run an effectful program, a handler for
+each required effect is supplied by the environment.
+
+These handlers both use AWS Simple Notification Service to interpret
+their respective effects. In their declarations, notice that each takes
+a stack of effects and pops off the type it handles. Popping off all
+the effects determines tot total interpration of an effecftul program.
+
+-}
 handleEMailWithAWS :: (LastMember IO effects) => Eff (EMail ': effects) a -> Eff effects a
 handleEMailWithAWS effs = interpretM impl effs
     where
         impl :: EMail a -> IO a
-        impl (DispatchEMailMessage msg) = sms topic msg
+        impl (DispatchEMailMessage msg) = sns topic msg
         topic :: Text
         topic = "arn:aws:sns:us-east-1:845290112482:deleteme-gjw-email-effects-demo"
 
@@ -47,12 +87,35 @@ handleSMSWithAWS :: (LastMember IO effects) => Eff (SMS ': effects) a -> Eff eff
 handleSMSWithAWS effs = interpretM impl effs
     where
         impl :: SMS a -> IO a
-        impl (DispatchSMSMessage msg) = sms topic msg
+        impl (DispatchSMSMessage msg) = sns topic msg
         topic :: Text
         topic = "arn:aws:sns:us-east-1:845290112482:deleteme-gjw-sms-effects-demo"
 
-sms :: Text -> Text -> IO ()
-sms topic message = do
+
+
+{-
+Effectful environments. These cobine effect handlers as needed to interpret
+a program. The first supplies an interpretation of EMail only.
+Thus it can run "notifyInteresting" but it cannot run "notifyEmergency."
+
+The second can run either of the effectful programs because it supplies
+interpretations of all the necessary effects for both (even though not all
+of them are used in all programs).
+
+-}
+runFriendlyNotifications :: Eff '[EMail, IO] a -> IO a
+runFriendlyNotifications effs = handleEMailWithAWS effs & runM
+
+runRudeNotifications :: Eff '[EMail, SMS, IO] a -> IO a
+runRudeNotifications effs = handleEMailWithAWS effs & handleSMSWithAWS & runM
+
+
+
+{-
+AWS SNS implementation used by both handlers
+-}
+sns :: Text -> Text -> IO ()
+sns topic message = do
     let req = SQS.publish message & SQS.pTopicARN .~ Just topic
     env <- AWS.newEnv AWS.Discover
     void $ AWS.runResourceT . AWS.runAWS env $ AWS.send req
--- test/Spec/Effects.hs
+++ test/Spec/Effects.hs
@@ -24,9 +24,9 @@ tests = testGroup "Effects" [
             actual = notifyInteresting message
 
         -- then
-        snd (runFriendly actual) @?= [message],
+        snd (runFriendly actual) @?= [message], -- sends a single email message
 
-    testCase "sends a text message" $ do
+    testCase "sends text and email messages" $ do
         let
         -- given
             message = "something bad happened!"
@@ -35,19 +35,36 @@ tests = testGroup "Effects" [
             actual = notifyEmergency message
 
         -- then
-        snd (runRude actual) @?= [message, message, message]
-        (snd . fst) (runRude actual) @?= [message]
+        snd (runRude actual) @?= [message, message, message] -- sends 3 text messages
+        (snd . fst) (runRude actual) @?= [message] -- sends a single email message
 
         ]
 
 
+{-
+Pure interpretations for testing. Notice that runFriendly cannot
+run programs that require the "SMS" effect.
 
+Effects are great for testing because we can supply any interpretations
+we need to support the tests: stubs, fakes, or mocks. Multiple
+interpretations for the same effect can be used to support mulpiple
+testing scenarios if needed
+
+
+-}
 runFriendly ::  Eff '[EMail] a -> (a, [Text])
 runFriendly effs = handleEmail effs & run
 
 runRude :: Eff '[EMail, SMS] a -> ((a, [Text]), [Text])
 runRude effs = handleEmail effs & handleSMS & run
 
+{-
+
+Pure handlers for the effects needed by the programs being tested.
+They are similar implementations for these test cases: record each
+sent message in a list and return list alongside the program result.
+
+-}
 handleEmail :: Eff (EMail ': effects) a -> Eff effects (a, [Text])
 handleEmail effs = runWriter $ reinterpret impl effs
     where
