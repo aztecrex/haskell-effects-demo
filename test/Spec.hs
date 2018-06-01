210a853124e5dca13615c4102544459d06b45f14
Greg Wiley <gwiley@cj.com>
2018-06-01 08:10:46 -0700

    remove tests for unimplemented module

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,13 +2,11 @@ module Main where
 
 import Test.Tasty (testGroup, TestTree, defaultMain)
 import qualified Spec.Effects as Effects (tests)
-import qualified Spec.Transformers as Transformers (tests)
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Haskell Effects" [
-        Effects.tests,
-        Transformers.tests
+        Effects.tests
     ]
